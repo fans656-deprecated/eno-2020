eb4c3a320510c4c6ca1b5f6a6c21da679932c84b
fans656 <fans656@yahoo.com>
2019-08-20 21:24:24 +0800

    2019-08-20 21:24:25

--- src/graph/start.sh
+++ src/graph/start.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker run -d --rm -p 4490:80 -v $(realpath ~/eno-graph):/root/eno-graph --name graph.fme graph.fme
+docker run -d --rm -p 4490:80 \
+    -v $(realpath ~/eno-graph):/root/eno-graph \
+    -v $(realpath ~/.ssh):/root/.ssh \
+    --name graph.fme graph.fme
