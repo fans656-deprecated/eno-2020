31898ff00b613d281ba20026fb549f67c871d242
fans656 <fans656@yahoo.com>
2019-09-03 00:06:10 +0800

    2019-09-03 00:06:10

--- stome/frontend/build.sh
+++ stome/frontend/build.sh
@@ -3,5 +3,6 @@ IMAGE_NAME="stome.fans656.me-frontend"
 DATA=/data
 DATA_OUT=$DATA/out
 CODE_OUT=/code/out
+rm -rf out
 docker build -t $IMAGE_NAME .
 docker run -d --rm -v $PWD:$DATA $IMAGE_NAME cp -r $CODE_OUT $DATA_OUT
